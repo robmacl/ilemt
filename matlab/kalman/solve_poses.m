a66832ae1f0b57f16776dd7e2e4d89beb3c43032
Rob MacLachlan <ram@ri.cmu.edu>
2021-09-21 17:03:45 -0400

    Tweak solve_poses.m script

--- matlab/kalman/solve_poses.m
+++ matlab/kalman/solve_poses.m
@@ -1,5 +1,8 @@
 trackfile = 'free_slide';
 data = read_track_file([trackfile '.trace']);
-options = track_options('cal_file_base', '../cal_9_1_premo_rotated_dipole/output/XYZ');
+options = track_options();
+options.cal_file_base = '../cal_9_1_premo_rotated_dipole/output/XYZ';
+options.hemisphere = 3;
+
 track = track_pose_solution(data, options);
 save([trackfile '_poses'], '-struct', 'track');
