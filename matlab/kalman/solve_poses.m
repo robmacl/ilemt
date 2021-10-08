b2ab96bef48b2e9bfc606e92b8993eadac252739
Rob MacLachlan <ram@ri.cmu.edu>
2021-10-08 15:31:31 -0400

    Fix to matlab/kalman/solve_poses.m

--- matlab/kalman/solve_poses.m
+++ matlab/kalman/solve_poses.m
@@ -2,14 +2,14 @@ function [outfile] = solve_poses (trackfile)
 % Read ilemt_ui .trace data and convert to poses in .mat file
   [path,name,ext] = fileparts(trackfile);
   if (isempty(ext))
-    ext = 'trace';
+    ext = '.trace';
   end
-  data = read_track_file([path filesep name ext]);
+  data = read_track_file([path name ext]);
   options = track_options();
   options.cal_file_base = '../cal_9_1_premo_rotated_dipole/output/XYZ';
   options.hemisphere = 3;
 
   track = track_pose_solution(data, options);
-  outfile = [path filesep name '_poses'];
+  outfile = [path name '_poses'];
   save(outfile, '-struct', 'track');
 end
