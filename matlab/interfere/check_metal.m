71828bb1f8887640f4d11f0b85026d6e86798348
Rob MacLachlan <ram@ri.cmu.edu>
2022-09-23 11:21:20 -0400

    Start of matlab/interfere/ for metal and EMI testing code

---
+++ matlab/interfere/check_metal.m
@@ -0,0 +1,14 @@
+options = interfere_options();
+
+prefix = [options.cal_directory options.cal_file_base];
+
+if (options.ishigh)
+  options.concentric_cal_file = [prefix '_concentric_hr_cal'];
+  cal = load_cal_file([prefix '_hr_cal']);
+else
+  options.concentric_cal_file = [prefix '_concentric_lr_cal'];
+  cal = load_cal_file([prefix '_lr_cal']);
+end
+
+[~, couplings] = read_cal_data(options);
+[poses, valid] = pose_solution(couplings, cal, options);
