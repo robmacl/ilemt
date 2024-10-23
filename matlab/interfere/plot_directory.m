cd650773f95172d48ebf3aa110c9f7e57dbb7870
Rob MacLachlan <robmacl@cmu.edu>
2024-10-23 13:54:13 -0400

    Updates to matlab/interfere/, changes to stage_calibration_todo demo.vi for 3D guidance logging.

--- matlab/interfere/plot_directory.m
+++ matlab/interfere/plot_directory.m
@@ -9,19 +9,26 @@ function plot_directory(fileDirectory, input_plot)
 % 
 % input_plot:
 %     Four essential sub-variables for specific plot that matches directory.
+    if input_plot{1}=="ilemt"
+        % Extract data from file to high and low carriers, do the validity check and print out the summary statistitcs 
+        [result_all.High, data] = ExtractData(fileDirectory, true);
+        [result_all.Low, data] = ExtractData(fileDirectory, false);
 
-    % Extract data from file to high and low carriers, do the validity check and print out the summary statistitcs 
-    [result_all.High, data] = ExtractData(fileDirectory, true);
-    [result_all.Low, data] = ExtractData(fileDirectory, false);
-    
-    % Process high carrier data to translation and rotation components
-    result_all.High = dataProcess(result_all.High);
-    % Process low carrier data to translation and rotation components
-    result_all.Low = dataProcess(result_all.Low);
+        % Process high carrier data to translation and rotation components
+        result_all.High = dataProcess(result_all.High);
+        % Process low carrier data to translation and rotation components
+        result_all.Low = dataProcess(result_all.Low);
 
+    elseif input_plot{1}== "Guidance"        
+        [result_all.High, data] = ExtractData_t2pose(fileDirectory);
+        result_all.High = dataProcess(result_all.High);
+        result_all.Low = result_all.High;
+        
+    end
+        
     % Process all parameters for plotting
     [input_param] = input_params(fileDirectory, data, input_plot);
-    
+
     % Main plotting function
     plot_graph(result_all, data, input_param);
 end
--- matlab/interfere/plotxmoving.m
+++ matlab/interfere/plotxmoving.m
@@ -2,21 +2,21 @@
 function plotxmoving(result_all, data, input_param)
     %% Parameter preparation and data extraction
     step = numel(result_all.High.transResult) / size(data, 1);
-    
+
     % Initialize an array of cells to store the split subset
     num_subsets = floor(length(result_all.High.transResult) / step);
     subsets = cell(1, num_subsets);
-    
+
     % Split data and convert to row vectors using for loops
     for i = 1:num_subsets
         start_idx = (i - 1) * step + 1;
         end_idx = i * step;
-        
+
         subset.High.trans = result_all.High.transResult(start_idx:end_idx);
         subset.High.rot = result_all.High.rotResult(start_idx:end_idx);
         subset.Low.trans = result_all.Low.transResult(start_idx:end_idx);
         subset.Low.rot = result_all.Low.rotResult(start_idx:end_idx);
-        
+
         subset_High_trans{i} = subset.High.trans';
         subset_High_rot{i} = subset.High.rot';
         subset_Low_trans{i} = subset.Low.trans';
@@ -34,22 +34,27 @@ function plotxmoving(result_all, data, input_param)
         idx.name = (data.MetalName(2:numel(subsets)/2));
     end
     
-    %% Create the x-moving plot    
-    shape_choice = {'Hollow', 'Solid'};
-    carrier_choice = {'High', 'Low'};
-    data_xmoving = {subset_High_trans,subset_Low_trans,subset_High_rot,subset_Low_rot};
-   
-    % Loop for doing all plots
-    for j = 1:2
-        for i = 1:2
-            % Identify shape and carrier types
-            idx.shape = shape_choice{i};
-            idx.carrier = carrier_choice{j};
-            
-            xmoving_errorPlot(idx, data_xmoving{j}, data_xmoving{j+2}, input_param)
-            xmoving_coupling(idx, result_all.(idx.carrier).coupling, step, input_param)
-        end
-    end
+    %% Create the x-moving plot  
+    shape_choice = {'Hollow', 'Solid'};       
+    carrier_choice.ilemt = {'High', 'Low'};
+    data_xmoving.ilemt = {subset_High_trans,subset_Low_trans,subset_High_rot,subset_Low_rot};    
+    carrier_choice.Guidance = {'High'};  
+    data_xmoving.Guidance = {subset_High_trans,subset_High_rot};
+
+    for j = 1:size(carrier_choice.(input_param.sensor),2)
+         for i = 1:size(shape_choice,2)
+             % Identify shape and carrier types
+             idx.shape = shape_choice{i};
+             idx.carrier = carrier_choice.(input_param.sensor){j};
+
+             if input_param.sensor == "ilemt"    
+                 xmoving_errorPlot(idx, data_xmoving.(input_param.sensor){j}, data_xmoving.(input_param.sensor){j+2}, input_param)
+                 xmoving_coupling(idx, result_all.(idx.carrier).coupling, step, input_param)
+             else
+                 xmoving_errorPlot(idx, data_xmoving.(input_param.sensor){j}, data_xmoving.(input_param.sensor){j+1}, input_param)
+             end
+         end
+    end   
 end
 
 %% Sub-function for translation and rotational error 
@@ -71,9 +76,9 @@ function xmoving_errorPlot(idx, data_trans, data_rot, input_param)
 % 
 % input_param:
 %     The struct of essential parameters. See input_params.m for more information
-
+    
     % Assign the axis limit
-    if input_param.deg == 0
+    if input_param.deg == 0 && input_param.sensor == "ilemt"
         limit.x_trans = [0 50];
         limit.y_trans = [1e-6 2e-2];
         limit.x_rot = [0 50];
@@ -82,7 +87,7 @@ function xmoving_errorPlot(idx, data_trans, data_rot, input_param)
         limit.x_trans = 'auto';
         limit.y_trans = 'auto';
         limit.x_rot = 'auto';
-        limit.y_rot = 'auto';         
+        limit.y_rot = 'auto';    
     end
     
     lim_axis = {limit.x_trans, limit.x_rot, limit.y_trans limit.y_rot};
@@ -91,8 +96,17 @@ function xmoving_errorPlot(idx, data_trans, data_rot, input_param)
     figure;
     % Loop for plotting translational and rotational errors
     for i = 1:2
+        if input_param.sensor == "ilemt"
+            title_detail = "All " +string(idx.shape)+ " Metals "+string(idx.carrier)+" Carrier Effects "+string(errorType{i})+" Error on y = "+string(input_param.y_axis(1))+" and Rotate "+string(input_param.deg)+" Degree";
+            name_file = "TransRotError_"+string(idx.shape)+"_"+string(idx.carrier)+".fig";
+        else
+            title_detail = "All " +string(idx.shape)+ " Metals of 3DGuidance trakSTAR with "+string(errorType{i})+" Error on y = "+string(input_param.y_axis(1))+" and Rotate "+string(input_param.deg)+" Degree";
+            name_file = "TransRotError_"+string(idx.shape)+".fig";
+        end
+        
         x_lim = lim_axis{i};
-        y_lim = lim_axis{i+2};
+        y_lim = lim_axis{i+2}; 
+        
         if i == 1 
             data = data_trans;
         else
@@ -100,11 +114,17 @@ function xmoving_errorPlot(idx, data_trans, data_rot, input_param)
         end
         subplot(2,1,i)
         for j = idx.(idx.shape)
-            semilogy(input_param.x_axis, data{j}, '.-', 'MarkerSize', 8)
+%             data{j}
+            if input_param.sensor == 'ilemt'
+                semilogy(input_param.x_axis, data{j}, '.-', 'MarkerSize', 8)
+            else
+                semilogy(input_param.x_axis, data{j}, '.-', 'MarkerSize', 8)
+            end
             hold on
         end
         grid on
-        title("All " +string(idx.shape)+ " Metals "+string(idx.carrier)+" Carrier Effects "+string(errorType{i})+" Error on y = "+string(input_param.y_axis(1))+" and Rotate "+string(input_param.deg)+" Degree")
+        
+        title(title_detail)
         ylabel(string(errorType{i})+' Error(m)') 
         xlabel('X Position(cm)') 
         legend(idx.name)
@@ -112,7 +132,8 @@ function xmoving_errorPlot(idx, data_trans, data_rot, input_param)
         ylim(y_lim);
     end
 
-    savefig(fullfile(input_param.directory, "TransRotError_"+string(idx.shape)+"_"+string(idx.carrier)+".fig"))
+    savefig(fullfile(input_param.directory, name_file))
+%     savefig(fullfile(input_param.directory, "TransRotError_"+string(idx.shape)+"_"+string(idx.carrier)+".fig"))
 end
 
 %% Sub-function for coupling magnitude plot
@@ -140,7 +161,11 @@ function xmoving_coupling(idx, data, step, input_param)
 
     figure;
     for j = idx.(idx.shape)
-        semilogy(input_param.x_axis, data((j-1)*step+1:j*step), '.-', 'MarkerSize', 8)
+        if input_param.sensor == 'ilemt'
+            semilogy(input_param.x_axis, data((j-1)*step+1:j*step), '.-', 'MarkerSize', 8)
+        else
+            plot(input_param.x_axis, data((j-1)*step+1:j*step), '.-', 'MarkerSize', 8)
+        end
         hold on
     end
     grid on
